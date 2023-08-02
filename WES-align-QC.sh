6b5f783913344be3c83e36af6c88cc0400b906ef
Dylan Ammons <dyammons95@gmail.com>
2023-08-02 15:28:17 -0400

    Update WES-align-QC.sh

--- WES-align-QC.sh
+++ WES-align-QC.sh
@@ -14,11 +14,16 @@ tmpDir="/scratch/alpine/$USER/tmp/"
 ### This is the path to the metadata -- can specify or it will be pulled from the sbatch file when you run this script
 metadata=$1
 
+### Ensure this command will generate a list of all sample names -- can run beforehand to test if desired
+(cd ${inputdir} && ls *.fq) | cut -d"_" -f1 | sort -u > samples.tmp
+
+
 ### Path to reference genome
 genomeFA="/projects/adh91@colostate.edu/references/Ensembl.CanFam3.1/Canis_lupus_familiaris.CanFam3.1.dna.toplevel.fa"
 
 #do you want to run QC before trimming files?
 runPREQC=FALSE
+runPOSTQC=TRUE
 
 #if samples are split across two lanes, this will concatinate the files if set to TRUE
 multiLane=TRUE
@@ -47,14 +52,13 @@ mkdir -p $tmpDir #also make tmp dir if not already in existance
 ####### META DATA #############
 
 
-# this is lane 1 for each sample
-sample_list1=( $(cut -f1 -d',' --output-delimiter=' ' $metadata) )
-
-# this is lane 2 for each sample
-sample_list2=( $(cut -f2 -d',' --output-delimiter=' ' $metadata) )
+while read line
+do
+     echo $line"_R1.fastq,"$line"_R2.fastq,"$line >> metadata.csv
+done < samples.tmp
 
 # this is the nickname to give the files
-names=( $(cut -f3 -d',' --output-delimiter=' ' $metadata) )
+names=( $(cut -f1 --output-delimiter=' ' samples.tmp) )
 
 
 ####### PIPELINE ##############
@@ -62,38 +66,40 @@ names=( $(cut -f3 -d',' --output-delimiter=' ' $metadata) )
 # update the user
 echo -e ">>> INPUT: This script will process files from the metafile:\n\t$metadata"
 echo -e ">>> PLAN: This script will process the sample files into the following names: "
-echo -e "\tSAMPLE1\tSAMPLE2\tNAMES"
+echo -e "NAMES"
 
-for (( counter=0; counter < ${#sample_list1[@]}; counter++ ))
+for (( counter=0; counter < ${#names[@]}; counter++ ))
 do
-    echo -e "\t${sample_list1[$counter]}\t${sample_list2[$counter]}\t${names[$counter]}"
+    echo -e "${names[$counter]}"
 done
 
 
-#### STEP0: before starting, concatinate files based on metadata file
-if [ $runPREQC == TRUE ]
-then
-  echo -e "\n>>> cat: concatinating files"
-  for (( counter=0; counter < ${#sample_list1[@]}; counter++ )) ### <<<<< add an echo step of the actual command
-  do
-    samplename=${names[$counter]}
-    sample1=${sample_list1[$counter]}
-    sample2=${sample_list2[$counter]}
-  
-    cmd0="cat ${inputdir}/${sample1} ${inputdir}/${sample2} > ${inputdir}/${samplename}.fastq"
-  
-    echo -e "\t$ ${cmd0}"
-    time eval $cmd0
-  done
-  
-else
-  echo -e "\n>>> skipping cat as experiment was not split across lanes"
-fi
+#### STEP0: before starting, concatinate (or rename) files based on metadata file
+echo -e "\n>>> cat: concatinating files"
+
+for (( counter=0; counter < ${#names[@]}; counter++ ))
+do
+	samplename=${names[$counter]}
+	
+	testNum=( $(ls ${inputdir}/${samplename}*R1* | wc -l ))
+	if [ $testNum > 1 ]
+	then
+    	cmd01="cat ${inputdir}/${samplename}_*R1* > ${inputdir}/${samplename}_R1.fastq"
+    	cmd02="cat ${inputdir}/${samplename}_*R2* > ${inputdir}/${samplename}_R2.fastq"
+	else
+		cmd01="mv ${inputdir}/${samplename}_*R1* ${inputdir}/${samplename}_R1.fastq"
+		cmd02="mv ${inputdir}/${samplename}_*R2* ${inputdir}/${samplename}_R2.fastq"
+	fi
+	  
+    	echo -e "\t$ ${cmd01}"
+    	echo -e "\t$ ${cmd02}"
+
+done
 
 
 if [ $runPREQC == TRUE ]
 then
-    #### STEP1: fastqc to determine quality & trim reads -- be aware this will run all files in the input directory; it pretty quick though
+    #### STEP1: fastqc to determine quality & trim reads -- be aware this will run all files in the input directory; its pretty quick though
     echo -e "\n>>> FASTQC: generating quality report"
     mkdir -p $outputdir"01_fastqc_pre"    
 
@@ -121,11 +127,20 @@ do
     sample1=${sample_list1[$counter]}
     sample2=${sample_list2[$counter]}
     
-    # execute trim_galore
-    cmd2="trim_galore --paired $inputdir/$sample1 $inputdir/$sample2 \
-    -o $outputdir"02_trim_galore" \
-    --basename $samplename \
-    -q 30 &"
+    # execute trim_galore +/- fastqc
+	if [ $runPOSTQC == TRUE ]
+	then
+		cmd2="trim_galore --paired $inputdir/$sample1 $inputdir/$sample2 \
+		-o $outputdir"02_trim_galore" \
+		--basename $samplename \
+		--fastqc \
+		-q 30 &" 
+	else
+		cmd2="trim_galore --paired $inputdir/$sample1 $inputdir/$sample2 \
+		-o $outputdir"02_trim_galore" \
+		--basename $samplename \
+		-q 30 &"
+	fi
     
     echo -e "\t$ ${cmd2}"
     time eval $cmd2
@@ -160,7 +175,7 @@ done
 
 # SAMTOOLS and BAMCOVERAGE: to convert .sam output to uploadable .bam and .wg files
 echo -e "\n>>> SAMTOOLS/BAMCOVERAGE: to convert files to uploadable _sort.bam and _sort.bam.bai files:"
-samout=$outputdir"03_samtools/"
+samout=$outputdir"04_samtools/"
 mkdir -p $samout
 
 for seqname in ${names[@]}
